472d0d0a9a4623a1cc4b2cf00b86c7736f3b7aa3
Rihards Mednis <mednisrihards@gmail.com>
2020-11-08 02:20:50 +0200

    Completely restyled and JS animations added

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const articles = document.querySelectorAll('.article');
+const heading1 = document.querySelector('.first');
+const heading2 = document.querySelector('.second');
+const navBar = document.querySelector('.nav');
+const navBtns = document.querySelector('.nav__btns');
+const burger = document.querySelector('.fa-bars');
+const cross = document.querySelector('.cross');
+const hero = document.querySelector('.hero');
+const windowHeight = window.innerHeight;
+
+gsap.from(hero, {opacity: 0, duration: 3});
+gsap.from(heading2, {opacity: 0, duration: .9, delay: 1, y: -150});
+gsap.from(heading1, {opacity: 0, duration: .9, delay: 1, y: -150});
+// gsap.from(navBar, {opacity: 0, duration: .9, delay: 0.5, y: -150});
+
+burger.addEventListener('click', () => {
+  navBtns.classList.toggle('nav-active'); 
+  burger.style.display = 'none';
+  cross.style.display = 'block';
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    burger.style.display = 'none';
+    cross.style.display = 'none';
+  } else {
+    burger.style.display = 'block';
+  }
+});
+
+cross.addEventListener('click', () => {
+  navBtns.classList.toggle('nav-active'); 
+  burger.style.display = 'block';
+  cross.style.display = 'none';
+});
+
+(function() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll('.article');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('fade-in-element');
+          element.classList.remove('hidden');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  })();
